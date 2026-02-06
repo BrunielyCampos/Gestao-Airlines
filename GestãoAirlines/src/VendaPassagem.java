3de132abc919e10acb3c7c928c6d7cad7884b7f2
BrunielyCampos <brunielycampos@gmail.com>
2026-02-06 10:44:42 -0300

    Adicionando mais funcionalidades em VendaPassagem

--- GestãoAirlines/src/VendaPassagem.java
+++ GestãoAirlines/src/VendaPassagem.java
@@ -8,31 +8,59 @@ public class VendaPassagem {
     private double preco;
 
 
-    public VendaPassagem(Passageiros passageiro, Avioes aviao, String assento, double preco){
-        this.passageiro = passageiro;
-        this.aviao = aviao;
-        this.assento = assento;
-        this.preco = preco;
+    public VendaPassagem(Passageiros passageiro, Avioes aviao, String assento, double preco, String origemViagem, String destinoViagem) {
+        this.setPassageiro(passageiro);
+        this.setAviao(aviao);
+        this.setAssento(assento);
+        this.setPreco(preco);
+        this.setOrigemViagem(origemViagem);
+        this.setDestinoViagem(destinoViagem);
+    }
+
+    public VendaPassagem() {
+       
+    }
+
+     public void setOrigemViagem(String origemViagem) {
+        if (origemViagem != null && !origemViagem.isEmpty()) {
+            this.origemViagem = origemViagem;
+        } else {
+            System.out.println("Origem inválida. Por favor, forneça uma origem válida");
+        }
     }
     
     public Passageiros getPassageiro() {
-        return passageiro;
+        return this.passageiro;
     } 
+
+    public void setPassageiro(Passageiros passageiro) {
+        if (passageiro != null) {
+            this.passageiro = passageiro;
+        } 
+        else {
+            System.out.println("Passageiro inválido. Por favor, forneça um passageiro válido");
+        }
+    }
+
+    public void setDestinoViagem(String destinoViagem) {
+        if (destinoViagem != null && !destinoViagem.isEmpty()) {
+            this.destinoViagem = destinoViagem;
+        } else {
+            System.out.println("Destino inválido. Por favor, forneça um destino válido");
+        }
+    }
     
     public String getDestinoViagem() {
-        return destinoViagem;
+        return this.destinoViagem;
     }
 
     public String getOrigemViagem() {
-        return origemViagem;
+        return this.origemViagem;
     }
 
-    public void setPassageiro(Passageiros passageiro) {
-        this.passageiro = passageiro;
-    }
 
     public Avioes getAviao() {
-        return aviao;
+        return this.aviao;
     }
 
 
@@ -41,28 +69,46 @@ public class VendaPassagem {
     }
 
     public String getAssento() {
-        return assento;
+        return this.assento;
     }
 
     public void setAssento(String assento) {
-        this.assento = assento;
+        if (assento != null && !assento.isEmpty()) {
+            this.assento = assento;
+        } else {
+            System.out.println("Assento inválido. Por   favor, forneça um assento válido");
+        }
+
     }
 
     public double getPreco() {
-        return preco;
+        return this.preco;
     }
 
     public void setPreco(double preco) {
-        this.preco = preco;
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("Preço inválido. O preço não pode ser negativo.");
+        }
     }
 
-    public void OrigemDaViagem(){
-        System.out.println("Origem: ");
+    public String OrigemDaViagem(){
+        if (this.origemViagem != null && !this.origemViagem.isEmpty()) {
+            System.out.println("Origem: " + this.origemViagem);
+        } else { 
+            System.out.println("Origem: Não definida");
+        }
+        return this.origemViagem;
     }
-
-    public String DestinoDaViagem(){
-       
-        return this.destinoViagem;
-    }
+ 
+   public void VenderPassagem() {
+        if (this.passageiro != null && this.aviao != null && this.assento != null && this.preco >= 0) {
+            System.out.println("Passagem vendida com sucesso para " + this.passageiro.getNome() + " no avião " + this.aviao.getModelo() + " com assento " + this.assento + " por R$" + this.preco);
+        } 
+        else {                
+            System.out.println("Não foi possível vender a passagem. Verifique os dados fornecidos.");
+        }
+   }
  
 }
